fc449b9e9accf95c44f9f9a9c37d59eef07e24a2
Illbones <95974326+Illbones@users.noreply.github.com>
2022-02-05 18:53:33 +0000

    API integration

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forcastHTML = `<div class="row">`;
@@ -50,6 +51,14 @@ function displayForecast() {
   forecastElement.innerHTML = forcastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "98c239163a7116dec4092fb1cdd1e807";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -71,8 +80,9 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", (alt = response.data.weather[0].description));
 
   celsiusTemp = Math.round(response.data.main.temp);
-}
 
+  getForecast(response.data.coord);
+}
 function search(city) {
   let apiKey = "98c239163a7116dec4092fb1cdd1e807";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -114,4 +124,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("New York");
-displayForecast();

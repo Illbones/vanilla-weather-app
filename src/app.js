39ea55daa6ed01fa49e7db0e1f65a76f1e0e16ef
Illbones <95974326+Illbones@users.noreply.github.com>
2022-02-05 19:20:26 +0000

    Forecast now working

--- src/app.js
+++ src/app.js
@@ -21,30 +21,44 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forcastHTML = `<div class="row">`;
-  let days = ["Thur", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forcastHTML =
-      forcastHTML +
-      `          
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forcastHTML =
+        forcastHTML +
+        `          
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img 
-          src="src/images/01d.png" 
+          src="src/images/${forecastDay.weather[0].icon}.png"
           alt="weather icon" 
           width="40px" 
         />
         <div class="weather-forecast-temp">
-          <span class="weather-forecast-temp-max">18º</span> |
-          <span class="weather-forecast-temp-min">12º</span>
+          <span class="weather-forecast-temp-max">${Math.round(
+            forecastDay.temp.max
+          )}º</span> |
+          <span class="weather-forecast-temp-min">${Math.round(
+            forecastDay.temp.min
+          )}º</span>
         </div>
       </div>
             
   `;
+    }
   });
 
   forcastHTML = forcastHTML + `</div>`;
@@ -52,10 +66,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "98c239163a7116dec4092fb1cdd1e807";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
